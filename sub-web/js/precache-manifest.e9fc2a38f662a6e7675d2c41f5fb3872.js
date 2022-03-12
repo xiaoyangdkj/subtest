1804d6bc9d7f67a2b4c5ff8d0dccbcf066b43192
xiaoyangdkj <p1732769230@gmail.com>
2022-03-12 19:19:32 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ sub-web/js/precache-manifest.e9fc2a38f662a6e7675d2c41f5fb3872.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "_redirects"
+  },
+  {
+    "revision": "f1583285c83058b86e96",
+    "url": "css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "e40946e79bd24847f01d",
+    "url": "css/chunk-vendors.53794358.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "5fabed250c04532f3ae5cddfae9ec243",
+    "url": "index.html"
+  },
+  {
+    "revision": "f1583285c83058b86e96",
+    "url": "js/app.2427c847.js"
+  },
+  {
+    "revision": "b1918bf2f00d86170115",
+    "url": "js/chunk-87bd93be.74c5062c.js"
+  },
+  {
+    "revision": "e40946e79bd24847f01d",
+    "url": "js/chunk-vendors.e3757cb5.js"
+  },
+  {
+    "revision": "3fa7348f187dae70fad03ee4cc4e0a4f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
